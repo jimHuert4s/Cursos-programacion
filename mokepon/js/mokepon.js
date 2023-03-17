4a7af8a5127c866561f9157276e08d8d9e4a0265
jimHuert4a <jimy_hm@hotmail.com>
2023-03-16 22:13:12 -0500

    Main

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,1 +1,10 @@
-alert("Hola JS")
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+
+function seleccionarMascotaJugador() {
+    alert('SELECCIONASTE TU MASCOTA')
+}
+
+window.addEventListener('load', iniciarJuego)
