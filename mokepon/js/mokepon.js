5a4de59ede26ca01d486c9182b12aae697ab0c67
jimHuert4a <jimy_hm@hotmail.com>
2023-03-17 20:42:04 -0500

    clase 22

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -7,11 +7,30 @@ function seleccionarMascotaJugador() {
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRtigueya = document.getElementById('ratigueya')
+    let spamMascotaJugador = document.getElementById('mascota-jugador')
 
-    if(inputHipodoge.checked){ alert('Elegiste a hipodoge')}
-    else if(inputCapipepo.checked){alert('Elegiste a Capipepo')}    
-    if(inputRtigueya.checked){alert('Elegiste a Ratigueya')}    
+    if(inputHipodoge.checked){spamMascotaJugador.innerHTML='Hipodoge'}
+    else if(inputCapipepo.checked){spamMascotaJugador.innerHTML='Capopepo'}    
+    else if(inputRtigueya.checked){spamMascotaJugador.innerHTML='Ratigueya'}    
     else{ alert('Debes elegir una mascota')}
+
+    seleccionarMascotaEnemigo()
+
 }
 
-window.addEventListener('load', iniciarJuego)
+function seleccionarMascotaEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+    let spamMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+        if (ataqueAleatorio == 1){spamMascotaEnemigo.innerHTML='Hipodoge' }
+        else if (ataqueAleatorio == 2){spamMascotaEnemigo.innerHTML='Capipepo'}
+        else {spamMascotaEnemigo.innerHTML='Ratigueya'}
+
+
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+window.addEventListener('load', iniciarJuego)                                       
